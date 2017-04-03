31b778dca9946afbda689c2b6388509aa2912f43
Amalia <maria.ibarguengoytia@despegar.com>
2017-04-02 22:25:06 -0300

    Manejo de constantes, excepciones, path.

--- TesinaPruebaConcepto/src/tesinapruebaconcepto/popup/actions/Traducir.java
+++ TesinaPruebaConcepto/src/tesinapruebaconcepto/popup/actions/Traducir.java
@@ -1,6 +1,5 @@
 package tesinapruebaconcepto.popup.actions;
 
-
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -18,25 +17,23 @@ import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.EcorePackage;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IActionDelegate;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IObjectActionDelegate;
 import org.eclipse.ui.IWorkbenchPart;
 
+
 import tesinapruebaconcepto.Activator;
+import tesinapruebaconcepto.exception.genericException;
 import transformation.OCL2LNR;
 
+public class Traducir extends AbstractTraducir implements IObjectActionDelegate {
 
-public class Traducir implements IObjectActionDelegate {
-
-	private Shell shell;
-	private EcorePackage ecorePackage;
-	
+	 // private static final Logger LOGGER = LoggerFactory.getLogger(Traducir.class);
+	  
 	/**
 	 * Constructor for Action1.
 	 */
@@ -55,106 +52,98 @@ public class Traducir implements IObjectActionDelegate {
 	 * @see IActionDelegate#run(IAction)
 	 */
 	public void run(IAction action) {
-		IEditorPart editor = Activator.getDefault().getWorkbench().getActiveWorkbenchWindow()
-				.getActivePage().getActiveEditor();
-		IEditorInput input= editor.getEditorInput();
+		IEditorPart editor = Activator.getDefault().getWorkbench().getActiveWorkbenchWindow().getActivePage()
+				.getActiveEditor();
+		IEditorInput input = editor.getEditorInput();
 		IFile sourceFile = input.getAdapter(IFile.class);
 
-		//Obtener nomber del projecto activo.
+		// Obtener nomber del projecto activo.
 		IProject myProject = sourceFile.getProject();
-		
-		//Crear carpeta Traducciones (solo si no existe)
-		IFolder filesFolder = myProject.getFolder("Traducciones");
-		if (!filesFolder.exists()){
+
+		// Crear carpeta Traducciones (solo si no existe)
+		IFolder filesFolder = myProject.getFolder(FOLDER_SRC_TRADUCCIONES);
+		if (!filesFolder.exists()) {
 			try {
 				filesFolder.create(false, true, null);
 			} catch (CoreException e) {
-				System.out.println("Creacion de carpeta Traducciones ha fallado.");
-				e.printStackTrace();
+				throw new genericException(
+						String.format("Creacion de carpeta Traducciones ha fallado. %s", e.getMessage()), e);
 			}
 		}
 		try {
 			this.traducirOCL(sourceFile);
 		} catch (Exception e) {
-			e.printStackTrace();
+			throw new genericException(e);
 		}
 	}
-	
 
 	/**
 	 * @see IActionDelegate#selectionChanged(IAction, ISelection)
 	 */
 	public void selectionChanged(IAction action, ISelection selection) {
-	   System.out.println("SelectionChanged");
+		System.out.println("SelectionChanged");
+		//LOGGER.info("SelectionChanged");
 	}
-	
-	//Recibe el .ocl file de entrada, deberia generar el oclas para pasarselo de entrada a  la trasformacion ATL
-	public void traducirOCL(IFile sourceFile) throws Exception {
-		
-		copyRequiredFiles(sourceFile); //Copia los archivos .ocl.oclas y .ecore a la carpeta models que se encuentra en el projecto Transformador.
 
-		//Transformacion en ATL 
-		String FilePathXmiStr = getATLModelsURL()+"OCL2LNR_Output.xmi";
-		String FilePathOclasStr = getATLModelsURL() + sourceFile.getName().replace("ocl.", ""); //por alguna razon remueve el .ocl del .ocl.oclas
-		
-		OCL2LNR.run("file://"+FilePathOclasStr, "file://"+FilePathXmiStr);
-	
-		//Traducir XMI usando Xtext
+	// Recibe el .ocl file de entrada, deberia generar el oclas para pasarselo
+	// de entrada a la trasformacion ATL
+	public void traducirOCL(IFile sourceFile) throws Exception {
+
+		// Copia los archivos .ocl.oclas y .ecore a la carpeta models que se encuentra en el projecto Transformador.
+		copyRequiredFiles(sourceFile); 
+
+		// Transformacion en ATL
+		String FilePathXmiStr = getATLModelsURL() + FILE_XMI;
+		// por alguna razon remueve el .ocl del .ocl.oclas
+		String FilePathOclasStr = getATLModelsURL() + sourceFile.getName().replace(CLEAN_OCL, "");
+
+		OCL2LNR.run(FILE + FilePathOclasStr, FILE + FilePathXmiStr);
+
+		// Traducir XMI usando Xtext
 		TraducirDesdeXtext xtext = new TraducirDesdeXtext();
 		FileInputStream f = new FileInputStream(FilePathXmiStr);
 		EObject result = xtext.parseXmi(f);
-		System.out.println(xtext.serialize(result));
-		String srcDir = (sourceFile.getLocation().removeLastSegments(2).toString())+"/";
-	    DateFormat df = new SimpleDateFormat("dd-MM-yy-HHmmss");
+		//System.out.println(xtext.serialize(result));
+		//LOGGER.info(xtext.serialize(result));
+		String srcDir = (sourceFile.getLocation().removeLastSegments(2).toString()) + BARRA_INVERTIDA;
+		DateFormat df = new SimpleDateFormat(FORMAT_DATE);
 		String now = df.format(new Date()).toString();
-		String outputFileName = srcDir +"Traducciones/"+ sourceFile.getName().replace(".ocl.oclas", "_"+now)+ ".lnr";
+		String outputFileName = srcDir + FOLDER_TRADUCCIONES
+				+ sourceFile.getName().replace(CLEAN_OCL_OCLAS, GUION_BAJO + now) + EXTENSION_LNR;
 		FileOutputStream os = new FileOutputStream(outputFileName);
 		xtext.writeLnr(os, result);
-		
-		//Remover archivos movidos al proyecto transformador (.ocl, .ecore, .xmi)
+
+		// Remover archivos movidos al proyecto transformador (.ocl, .ecore, .xmi)
 		Files.deleteIfExists(Paths.get(FilePathXmiStr));
 		Files.deleteIfExists(Paths.get(FilePathOclasStr));
-		Files.deleteIfExists(Paths.get(getATLModelsURL() + sourceFile.getName().replace("ocl.", "")));
-		
-		//Refresh del projecto de usuaio para mostrar el nuevo archivo generado.
+		Files.deleteIfExists(Paths.get(getATLModelsURL() + sourceFile.getName().replace(CLEAN_OCL, VACIO)));
+
+		// Refresh del projecto de usuaio para mostrar el nuevo archivo generado.
 		sourceFile.getProject().refreshLocal(IResource.DEPTH_INFINITE, null);
 	}
 
-	private void copyRequiredFiles(IFile sourceFile) throws IOException
-	{ 
-		//Determinar path del proyecto de transformacion ATL
+	private void copyRequiredFiles(IFile sourceFile) throws IOException {
+		// Determinar path del proyecto de transformacion ATL
 		String destDir = getATLModelsURL();
-	
-		//Determinar path del projecto que contiene el archivo .ocl.oclas
-		String srcDir = (sourceFile.getLocation().removeLastSegments(1).toString())+"/";
-		
-		//Mover el archivo ocl.oclas
-		Files.copy(Paths.get(srcDir + sourceFile.getName()), Paths.get(destDir + sourceFile.getName().replace("ocl.", "")) , StandardCopyOption.REPLACE_EXISTING);
 
-		//mover el archivo .ecore  que posee el mismo nombre que el archivo ocl.oclas
-		String sourceStr = sourceFile.getName().split(".ocl")[0].concat(".ecore");
-		Files.copy( Paths.get(srcDir + sourceStr),  Paths.get(destDir + sourceStr) , StandardCopyOption.REPLACE_EXISTING);
+		// Determinar path del projecto que contiene el archivo .ocl.oclas
+		String srcDir = (sourceFile.getLocation().removeLastSegments(1).toString()) + BARRA_INVERTIDA;
+
+		// Mover el archivo ocl.oclas
+		Files.copy(Paths.get(srcDir + sourceFile.getName()),
+				Paths.get(destDir + sourceFile.getName().replace(CLEAN_OCL, VACIO)), StandardCopyOption.REPLACE_EXISTING);
+
+		// mover el archivo .ecore que posee el mismo nombre que el archivo
+		// ocl.oclas
+		String sourceStr = sourceFile.getName().split(EXTENSION_OCL)[0].concat(EXTENSION_ECORE);
+		Files.copy(Paths.get(srcDir + sourceStr), Paths.get(destDir + sourceStr), StandardCopyOption.REPLACE_EXISTING);
 	}
 
 	private String getATLModelsURL() {
-		return Platform.getBundle("OCL2LNR_Transformador").getLocation().replaceAll("reference:file:/", "").concat("models/");
+		return Platform.getBundle(PROJECT_TRANSFORMADOR).getLocation().replaceAll(REFERENCE_FILE, VACIO)
+				.concat(FOLDER_MODELS);
 	}
 
-	public Shell getShell() {
-		return shell;
-	}
 	
 
-	public void setShell(Shell shell) {
-		this.shell = shell;
-	}
-
-	public EcorePackage getEcorePackage() {
-		return ecorePackage;
-	}
-
-	public void setEcorePackage(EcorePackage ecorePackage) {
-		this.ecorePackage = ecorePackage;
-	}
-	
 }
